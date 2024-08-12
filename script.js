a0d8f2b551d6462c0c0afeec28717d2079d1c6a1
Eduardo Augusto <163478183+EduardoAugustoBF@users.noreply.github.com>
2024-08-12 22:09:35 +0100

    terminando o projeto

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-console.log("Deu bom rapaz")
+let list = document.querySelectorAll('.item')
+let next = document.getElementById('next')
+let prev = document.getElementById('prev')
+
+// count = 3
+// 0, 1, 2
+//
+let count = list.length
+let active = 0 
+
+console.log(list)
+
+// Arrow function
+next.onclick = () => {
+    let activeOld = document.querySelector('.active')
+    activeOld.classList.remove('active')
+
+    active = active >= count -1 ? 0 : active + 1
+    list[active].classList.add('active')
+}
+
+prev.onclick = () => {
+    let activeOld = document.querySelector('.active')
+    activeOld.classList.remove('active')
+}
